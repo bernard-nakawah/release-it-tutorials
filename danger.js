be70cb7d8144a0424ad1576eb9ae4b3daef22b5c
bernard-nakawah <bnakawah@chippercash.com>
2022-12-07 11:17:48 +0000

    fix: updated danger

---
+++ danger.js
@@ -0,0 +1,6 @@
+import { message, danger } from "danger";
+
+const modifiedMD = danger.git.modified_files.join("- ");
+message("Changed Files in this PR: \n - " + modifiedMD);
+
+const hellow = "em";
